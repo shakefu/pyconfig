194948fccbf808169ba8a86cd3960d3e730afcbb
Jacob Alheid <shakefu@gmail.com>
2012-07-29 15:00:33 -0700

    Fix setup.py to use README.rst as long_description.

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
+import os
 from setuptools import setup, find_packages
+
+
+def README():
+    try:
+        return open(os.path.join(os.path.dirname(__file__),
+            'README.rst')).read()
+    except:
+        raise
+        pass
+
 
 setup(
         name='pyconfig',
-        version='0.1.0-dev',
+        version='1.0.1',
         description="Python-based singleton configuration",
+        long_description=README(),
         author="Jacob Alheid",
         author_email="jake@about.me",
         url="http://github.com/shakefu/pyconfig",
