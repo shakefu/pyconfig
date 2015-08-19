280cdbf0df8e96f63b7eafd0a8c4f88213d071fd
Jacob Alheid <shakefu@gmail.com>
2015-08-19 16:40:51 -0700

    Fix setup.py for version stuff.

--- setup.py
+++ setup.py
@@ -1,13 +1,29 @@
 import multiprocessing, logging # Fix atexit bug
 from setuptools import setup, find_packages
 
-import pyconfig
+
+def readme():
+    try:
+        return open('README.rst').read()
+    except:
+        pass
+    return ''
+
+
+def version():
+    try:
+        import re
+        return re.search("^__version__ = '(.*)'",
+                open('pyconfig/__init__.py').read(), re.M).group(1)
+    except:
+        raise RuntimeError("Could not get version")
 
 
 setup(
         name='pyconfig',
-        version=pyconfig.__version__,
+        version=version(),
         description="Python-based singleton configuration",
+        long_description=readme(),
         author="Jacob Alheid",
         author_email="shakefu@gmail.com",
         url="http://github.com/shakefu/pyconfig",
