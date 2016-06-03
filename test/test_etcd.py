a057496e0fe02e91044dfc5c3d983a3512df49f6
Jacob Alheid <shakefu@gmail.com>
2016-06-03 14:39:34 -0700

    test: Watching tests.

--- test/test_etcd.py
+++ test/test_etcd.py
@@ -1,3 +1,6 @@
+import os
+import time
+
 import pytool
 from nose import SkipTest
 from nose.tools import ok_, eq_
@@ -31,6 +34,7 @@ def teardown():
     # Clean up the test namespace
     pyconfig.etcd().client.delete('pyconfig_test/test', dir=True, recursive=True)
     pyconfig.etcd().client.delete('pyconfig_test/test2', dir=True, recursive=True)
+    pyconfig.etcd().client.delete('pyconfig_test/watching', dir=True, recursive=True)
     pyconfig.etcd().client.delete('pyconfig_test/', dir=True, recursive=True)
 
 
@@ -88,4 +92,35 @@ def test_autoloading_etcd_config_works():
     pyconfig.reload()
     eq_(pyconfig.get('pyconfig.string'), 'Value')
     eq_(pyconfig.get('pyconfig.number'), 2)
+
+
+def test_watching():
+    # Enable watching
+    os.environ['PYCONFIG_ETCD_WATCH'] = 'true'
+
+    pyconfig.Config().clear()
+    pyconfig.set('pyconfig.etcd.prefix', 'pyconfig_test/watching')
+    pyconfig.reload()
+
+    if not pyconfig.etcd().watching:
+        raise SkipTest
+
+    # Wait for 20ms before writing to ensure the watcher thread is ready
+    time.sleep(0.020)
+
+    # Write a new value directly to etcd
+    pyconfig.etcd().client.write('pyconfig_test/watching/it.works', 
+            pytool.json.as_json(True))
+
+    # Try to get the value... this is a bit sloppy but good luck doing
+    # something better
+    retry = 50
+    while retry:
+        retry -= 1
+        if pyconfig.get('it.works', None) is not None:
+            break
+        # Wait 20ms more for it to show up
+        time.sleep(0.020)
+
+    eq_(pyconfig.get('it.works', False), True)
 
