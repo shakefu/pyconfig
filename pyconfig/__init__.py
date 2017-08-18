b8491bc36f6341fdbb621fa59b3e641b3c034cef
Jacob Alheid <shakefu@gmail.com>
2017-08-17 21:42:16 -0700

    pyconfig: Add secret support for jetconfig.

--- pyconfig/__init__.py
+++ pyconfig/__init__.py
@@ -359,6 +359,8 @@ class etcd(object):
         if not self.module:
             return
 
+        self._parse_jetconfig()
+
         # Check env for overriding configuration or pyconfig setting
         hosts = env('PYCONFIG_ETCD_HOSTS', hosts)
         protocol = env('PYCONFIG_ETCD_PROTOCOL', None)
@@ -510,6 +512,48 @@ class etcd(object):
 
         # The python-etcd client explicitly checks for a tuple type
         return tuple(hosts)
+
+    def _parse_jetconfig(self):
+        """
+        Undocumented cross-compatability functionality with jetconfig
+        (https://github.com/shakefu/jetconfig) that is very sloppy.
+
+        """
+        conf = env('JETCONFIG_ETCD', None)
+
+        if not conf:
+            return
+
+        import urlparse
+
+        auth = None
+        port = None
+        conf = conf.split(',').pop()
+        entry = urlparse.urlparse(conf)
+        scheme = entry.scheme
+        host = entry.netloc or entry.path # Path is where it goes if there's no
+                                          # scheme on the URL
+
+        if '@' in host:
+            auth, host = host.split('@')
+
+        if ':' in host:
+            host, port = host.split(':')
+
+        if not port and scheme == 'https':
+            port = 443
+
+        if scheme:
+            os.environ['PYCONFIG_ETCD_PROTOCOL'] = scheme
+
+        if auth:
+            os.environ['PYCONFIG_ETCD_AUTH'] = auth
+
+        if port:
+            host = host + ":" + port
+
+        os.environ['PYCONFIG_ETCD_HOSTS'] = host
+
 
     # Getter and setter for the prefix to ensure it stays sync'd with the
     # config and stays normalized
